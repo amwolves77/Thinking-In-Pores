898b61a6db3dc720d111ee440637f75641c21656
amwolves77 <127142570+amwolves77@users.noreply.github.com>
2026-01-05 18:09:40 +1000

    Import App component from App.tsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
-function App() {
-  return <h1>Hello from my app ✅</h1>;
-}
+import App from "../App.tsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
