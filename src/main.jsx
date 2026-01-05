2b5689a6dfdc449a851990690fcbf5e7cd67b7ff
amwolves77 <127142570+amwolves77@users.noreply.github.com>
2026-01-05 18:04:14 +1000

    Fix import path for App component in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./main.jsx";
+
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
